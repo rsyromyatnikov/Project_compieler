31a8ef93de5eed260429974b969b76be6ce542df
Fedos <fedyaq1@gmail.com>
2013-04-18 18:21:44 +0300

    Add test message

--- src/main/java/com/magistr/Lexer/HelloWorldLexer.java
+++ src/main/java/com/magistr/Lexer/HelloWorldLexer.java
@@ -7,7 +7,7 @@ public class HelloWorldLexer {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("Test");
 	}
 
 }
