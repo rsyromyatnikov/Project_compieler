2e04c522133e4732d44ac7e998289c7fd3d1164f
Roma <Rimus91gmail.com>
2013-06-03 22:39:45 +0300

    Added Node for AST

--- src/main/java/com/magistr/Parser/Node.java
+++ src/main/java/com/magistr/Parser/Node.java
@@ -1,5 +1,64 @@
 package com.magistr.Parser;
 
-public class Node {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.magistr.Lexer.Token;
+
+public class Node {
+	private Token token;
+	private int level;
+	private List<Node> children;
+
+	public Node(Token token){
+		this.token = token;
+		this.level = 0;
+		this.children = new ArrayList<Node>(); ;
+	}
+	
+	public void add (Token token){
+		addNode(new Node(token));
+	}
+	
+	public void addNode(Node node){
+		setLevel(getLevel()+1);
+		children.add(node);
+	}
+
+	public Token getToken() {
+		return token;
+	}
+
+	public void setToken(Token token) {
+		this.token = token;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public List<Node> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<Node> children) {
+		this.children = children;
+	}
+	
+	public String toString(){
+		String s = "     " + getLevel();
+		if (getToken() == null){
+			s+= "ROOT\n";
+		} else {
+			s+= getToken().getCargo() + "\n";
+		}
+		for (Node n:children){
+			s+= n.toString();
+		}
+		return s;
+	}
 }
--- src/main/java/com/magistr/Parser/ParserException.java
+++
@@ -1,5 +0,0 @@
-package com.magistr.Parser;
-
-public class ParserException {
-
-}
