ca8cd8172e38c0e6fc6b5d81c724ad4eba7ecfa0
Roma <Roma@10.107.48.20>
2013-06-04 18:30:42 +0300

    Added methods to Parser

--- src/main/java/com/magistr/Parser/Parser.java
+++ src/main/java/com/magistr/Parser/Parser.java
@@ -1,5 +1,61 @@
 package com.magistr.Parser;
 
-public class Parser {
+import com.magistr.Lexer.Lexer;
+import com.magistr.Lexer.Token;
 
+public class Parser {
+	
+	private Lexer lx;
+	private Token token = null;
+	private boolean verbose = false;
+	private int indent = 0;
+	private String[] numberOperator = {"+","-","/","*"};
+	
+	public Parser(String sourceText) throws Exception{
+		lx = new Lexer(sourceText);
+	}
+	
+	public void getToken() {
+		if (verbose) {
+			System.out.println(mulString(" ",indent) 
+					+ "  ("+ token.toString() + ")");
+		}
+		token = lx.get();
+	}
+	
+	public void push(String s){
+		indent+=1;
+		if (verbose){
+			System.out.println(mulString(" ",indent) + " " +s);
+		}
+	}
+	
+	public void pop(String s){
+		if (verbose){
+			//System.out.println(mulString(" ",indent) + " " +s+ ".end");
+		}
+		indent -=1;
+	}
+	
+	// track0 method 
+	// track method 
+	
+	
+	
+	
+	
+	
+	private static String mulString(String s, int times){
+   	 String buf= "";
+   	 for (int i=0;i< times; i++){
+   		 buf+=s;
+   	 }   	 
+   	 return  buf;    	 
+    }
+
+	
+
+	
+	
+	
 }
--- src/main/java/com/magistr/Parser/ParserDriver.java
+++ src/main/java/com/magistr/Parser/ParserDriver.java
@@ -1,5 +1,15 @@
 package com.magistr.Parser;
 
 public class ParserDriver {
-
+     public static void main(String[] args){
+    	 System.out.println(mulString("1",1));
+     }
+     
+     private static String mulString(String s, int times){
+    	 String buf= "";
+    	 for (int i=0;i< times; i++){
+    		 buf+=s;
+    	 }   	 
+    	 return  buf;    	 
+     }
 }
